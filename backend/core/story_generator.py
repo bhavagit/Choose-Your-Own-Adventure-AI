4228968d72835d54065d19cb831791b6e3d079a0
bhavagit <bavadharini@gmail.com>
2025-12-12 15:11:15 -0500

    update backend for openai service

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -18,11 +18,14 @@ class StoryGenerator:
 
     @classmethod
     def _get_llm(cls):
-        openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
-        serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
 
-        if openai_api_key and serviceurl:
-            return ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key, base_url=serviceurl)
+        serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
+        consumerkey = os.getenv("CHOREO_OPENAI_CONNECTION_CONSUMERKEY")
+        consumersecret = os.getenv("CHOREO_OPENAI_CONNECTION_CONSUMERSECRET")
+        tokenurl = os.getenv("CHOREO_OPENAI_CONNECTION_TOKENURL")
+
+        if consumerkey and serviceurl:
+            return ChatOpenAI(model="gpt-4o-mini", api_key=consumerkey, base_url=serviceurl)
 
         return ChatOpenAI(model="gpt-4o-mini")
 
